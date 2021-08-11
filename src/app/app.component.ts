ac10bafd68e594f830b70247c9be8dd35ed21a1c
mazaraki <dimitramazaraki@gmail.com>
2021-08-11 23:56:28 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  username: string = '';
+  isClicked: boolean = false;
+
+  onClicked() {
+    return (this.isClicked = this.username.length !== 0);
+  }
+
+  onReset() {
+    this.username = '';
+    this.isClicked = !this.isClicked;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './SuccessAlert/success-alert.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [
     AppComponent,
-    HelloComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    HelloComponent
   ],
   bootstrap: [AppComponent]
 })
